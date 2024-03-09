517e602114909a0dce69d33646d086267307bd8e
NguyenHuuVan17A2DHKL <nhvan.dhkl17a2hn@sv.uneti.edu.vn>
2024-03-09 13:02:40 +0700

    chú thích

---
+++ 9.7.py
@@ -0,0 +1,13 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def phan_nguyen(a: int, b: int) -> int:
+...     return a//b
+... 
+>>> phan_nguyen(8,3)
+2
+>>> phan_nguyen(15,6)
+2
+>>> phan_nguyen(21,2)
+10
+>>> phan_nguyen(56,3)
+18
