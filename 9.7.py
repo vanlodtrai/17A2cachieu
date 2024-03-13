a0331bb0a454c53bc50aac0773e2ffb860426344
NguyenHuuVan17A2DHKL <nhvan.dhkl17a2hn@sv.uneti.edu.vn>
2024-03-14 00:46:28 +0700

    hh

--- 9.7.py
+++ 9.7.py
@@ -1,13 +1,36 @@
-Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def phan_nguyen(a: int, b: int) -> int:
-...     return a//b
-... 
->>> phan_nguyen(8,3)
-2
->>> phan_nguyen(15,6)
-2
->>> phan_nguyen(21,2)
-10
->>> phan_nguyen(56,3)
-18
+#Bài 2
+"""
+Nhập các giá trị: s tính bằng giây, m tính bằng phút, h tính bằng giờ, d tính bằng ngày. 
+Viết công thức đổi giá trị “”d ngày, h giờ, m phút, s giây” sang đơn vị giây.
+"""
+#s,m, h, d = map(int, input("Nhập các số đo: ").split())
+#d_s = d * 24 * 60 * 60
+#h_s = h * 60 * 60
+#m_s = m * 60
+#thoi_gian = d_s + h_s + m_s + s
+#print("Chuyển sang giây được: ", thoi_gian)
+
+"""
+Diện tích xung quanh = chu vi đáy x chiều cao
+Diện tích toàn phần = diện tích xung quanh + diện tích 2 đáy
+Thể tích = diện tích đáy x chiều cao
+"""
+
+#Bài 4
+# a = (x1,y1) b = (x2,y2). Tích vô hướng 2 vecto: a.b = x1.x2 + y1.y2
+
+#Bài 5
+# Giá điện = Công suất x giá 1 số
+# Công suất = Hiệu điện thế x cường độ dòng điện
+
+#Bài 8
+# Điểm A(x,y,z)
+# Tọa độ điểm đối xứng qua mặt phẳng Oxy: (-x,-y,z)
+# Tọa độ điểm đối xứng qua mặt phẳng Oxz: (-x,y,-z)
+# Tọa độ điểm đối xứng qua mặt phẳng Oyz: (x, -y,-z)
+#x,y,z = map(int, input("Nhập tọa độ đỉnh A:").split())
+#print("Điểm đối xứng với A qua mặt phẳng Oxy là: ({},{},{})".format(-x, -y, z))
+
+#Bài 9
+#log = math.log(x, 4) #Logarit cơ số 4 của x
+#log2 = math.log(2,x) #Logarit cơ số x của 2
